2a41a8ceebe819cc3fc2f247142b9b42e1f73ba1
Demian <dem@ciencias.unam.mx>
2020-11-29 15:16:10 -0600

    clase CircularImage

--- src/main/java/com/unam/ciencias/modelado/cloudcoverage/Run.java
+++ src/main/java/com/unam/ciencias/modelado/cloudcoverage/Run.java
@@ -3,7 +3,7 @@ package com.unam.ciencias.modelado.cloudcoverage;
 public class Run {
 
     public void run(String[] args) {
-        System.out.println("Estoy listo para trabajar. :)");
+
     }
     
 }
