3f5ed188f4ce38d3f3e5c87da63e112ff07d6e79
carlosandradeh <carlosandrade@ciencias.unam.mx>
2020-12-04 17:59:42 -0600

    Documentación lista

--- src/main/java/com/unam/ciencias/modelado/cloudcoverage/CloudCoverageIndex.java
+++ src/main/java/com/unam/ciencias/modelado/cloudcoverage/CloudCoverageIndex.java
@@ -2,6 +2,12 @@ package com.unam.ciencias.modelado.cloudcoverage;
 
 public class CloudCoverageIndex {
 
+    /**
+     * Method con Calculate the Cloud Covergae index.
+     * @param cloudArea The total area of the clouds of a image. 
+     * @param totalArea The total area of the circle of a image.
+     * @return float index.
+     */
     public static float getIndex(float cloudArea, float totalArea) {
         if (totalArea <= 0)
             return 0;
--- src/main/java/com/unam/ciencias/modelado/cloudcoverage/Main.java
+++ src/main/java/com/unam/ciencias/modelado/cloudcoverage/Main.java
@@ -1,6 +1,10 @@
 package com.unam.ciencias.modelado.cloudcoverage;
 
 public class Main {
+    /**
+     * Main method of the program, is used to run the program.
+     * @param args The arguments of the program such as the image.jpg or the flag S or s.
+     */
     public static void main(String[] args) {
         Run cloudCoverage = new Run();
         cloudCoverage.run(args);
